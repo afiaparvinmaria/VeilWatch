e0a15389c0c13f046c32f8ede94ddb2e6ab3c972
Mohima Akter Reem (mohima19) <shehnaazmohee12@gmail.com>
2024-12-10 01:04:22 +0600

    Add files via upload

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
-from .models import CrimeReport
+from django.contrib.auth.admin import UserAdmin
+from .models import Location, Crime, appUser
 
-@admin.register(CrimeReport)
-class CrimeReportAdmin(admin.ModelAdmin):
-    list_display = ('location', 'crime_type', 'intensity', 'latitude', 'longitude')
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('location_id', 'city', 'division')
+    search_fields = ('city', 'division')
+    list_filter = ('city', 'division')
+
+@admin.register(Crime)
+class CrimeAdmin(admin.ModelAdmin):
+    list_display = ('crime_id', 'type', 'date_time', 'address', 'latitude', 'longitude', 'location')
+    search_fields = ('type', 'description')
+    list_filter = ('type', 'date_time', 'location', 'status')
+    autocomplete_fields = ['location']
+
+@admin.register(appUser)
+class CustomUserAdmin(UserAdmin):
+    # You can display custom fields here for the User model
+    list_display = ('username', 'email', 'role', 'last_login', 'date_joined')
+    list_filter = ('role', 'is_active', 'last_login')  # Use valid filter fields
+    search_fields = ('username', 'email')
